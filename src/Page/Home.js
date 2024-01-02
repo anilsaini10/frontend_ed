23494301dec08dc34dd0989ae34159160efcfbb8
anilsaini10 <anilsainik10@gmail.com>
2024-01-02 17:28:50 +0530

    changed endpoint

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -21,7 +21,7 @@ const CreateAdminUser = () => {
 
         try {
 
-            let res = await axios.get("https://dzap-f4vc.onrender.com/getList");
+            let res = await axios.get("https://dzap-f4vc.onrender.com");
 
             if (res?.status == 200) {
                 console.log(res?.data);
